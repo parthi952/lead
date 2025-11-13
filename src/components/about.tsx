0dba7fa86de5bb4878c40abea70aabc2864405c1
parthimp952 <parthimp952@gmail.com>
2025-11-13 09:53:39 +0530

    Initial commit

---
+++ src/components/about.tsx
@@ -0,0 +1,37 @@
+export default function About() {
+    return (
+        <div>
+            {/* About page main */}
+        <div className="mt-25 bg-blue-100 relative w-auto h-auto overflow-hidden overflow-x-hidden">
+            <h4>A Few Words About</h4>
+            <h1>Our Company</h1>
+            <h3>Transforming Today, Tomorrow, Forever</h3>
+        </div>
+
+        {/* about us */}
+        <div>
+            <h1>About Us</h1>
+            <h1>LEADING THE WAY WHERE EXCELLENCE MEETS INNOVATION</h1>
+            <hr />
+            <p><span>L</span>eadoptima, a visionary startup, passionately offers an extensive suite of services to our esteemed clientele. From cutting-edge information technology solutions and premium engineering services to expert staffing solutions and transformative skill development programs, we are steadfast in our commitment to exceeding expectations with unparalleled excellence and innovative prowess</p>
+        <div>
+            <h1>Our Vision</h1>
+            <p>To revolutionize industries through innovation in Information Technology, Engineering Services, and Human Resources, empowering businesses and individuals alike. As pioneers, we aim to shape a world where technology serves humanity, engineering propels advancement, and HR fosters individual excellence. </p>
+        </div>
+        <div>
+            <h1>Our Mission</h1>
+            <p>To pioneer bespoke IT, Engineering, and HR solutions, catalyzing innovation and fostering exponential growth. With unwavering dedication to excellence, we empower clients to unleash their fullest potential. Leadoptima: Your catalyst for transformative progress.</p>
+        </div>
+        </div>
+        {/* why us */}
+        <div>
+            <h1>Unlock Customized Solutions for Your Unique Business Needs</h1>
+            <div>
+                <h1>Tailored Solutions</h1>
+                <p>Discover how Leadoptima can tailor its cutting-edge IT services, dynamic engineering solutions, skill development programs, and reliable staffing solutions to perfectly align with your business's unique challenges and objectives.</p>
+            </div>
+            
+        </div>
+        </div>
+    )
+}
